6582d91b80dcfd216923c14ee7844b4db3a96087
quitefabulous <35996042+quitefabulous@users.noreply.github.com>
2023-02-04 15:10:19 +0100

    setup inner tree for now

--- Assets/Scripts/Stations/HealingStation.cs
+++ Assets/Scripts/Stations/HealingStation.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiggingStation : Station
+public class HealingStation : Station
 {
     protected override void OnPlayerUse()
     {
-        Debug.Log("Digging");
+        Debug.Log("Healing");
     }
 }
