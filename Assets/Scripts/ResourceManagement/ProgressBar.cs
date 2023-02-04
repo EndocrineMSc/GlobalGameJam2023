ef3d28bde35caa8fb678ae355ffd9b672e54a8a8
BAMSc Laptop <bamsc.studio@gmail.com>
2023-02-04 18:47:18 +0100

    ResourceStation and most relevant logic implemented

--- Assets/Scripts/ResourceManagement/ProgressBar.cs
+++ Assets/Scripts/ResourceManagement/ProgressBar.cs
@@ -10,41 +10,22 @@ namespace GameName.ResourceHandling
     {
         #region Fields
 
-        [SerializeField] private Slider _healthFill;
-        private 
+        [SerializeField] private Slider _resourceFill;
+        private InteractableStation _interactableStation;
 
         #endregion
 
         #region Functions
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-            
+            _interactableStation = GetComponent<InteractableStation>();
+            _resourceFill.maxValue = _interactableStation.TargetResourceAmount;
         }
 
-        // Update is called once per frame
         void Update()
         {
-            /*
-            _healthFill.value = _player.Health;
-            _shieldFill.value = _player.Shield;
-
-            if (_player.Shield > 0)
-            {
-                _shield.enabled = true;
-                _heart.enabled = false;
-                _shieldText.enabled = true;
-
-                _shieldText.text = _player.Shield.ToString();
-            }
-            else
-            {
-                _shield.enabled = false;
-                _heart.enabled = true;
-                _shieldText.enabled = false;
-            }
-            */
+            _resourceFill.value = _interactableStation.CurrentResourceAmount;
         }
 
         #endregion
